e0fb78a8dfb89d49c87ab7a613c159c52ba59a58
Su-Hyun Park <lovehyun@hotmail.com>
2025-07-10 14:47:05 +0900

    pixabay 의 admin 페이지 template 엔진 버전으로 완료

--- 5.Project/2.pixabay/4.frontend_admin_template/app.py
+++ 5.Project/2.pixabay/4.frontend_admin_template/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, jsonify, url_for, render_template, request
+from flask import Flask, jsonify, redirect, url_for, render_template, request
+import os
 
 app = Flask(__name__)
 
+# 글로벌 변수
 images = [
     {"filename":"cat1.jpg", "keywords": ["cat", "animal", "cute"]},
     {"filename":"cat2.jpg", "keywords": ["cat", "pet", "cute"]},
@@ -12,30 +14,64 @@ images = [
 
 @app.route('/')
 def index():
-    return render_template("index.html")
-
-@app.route('/search')
-def search():
     query = request.args.get("q", "").lower()
     results = []
     
     for item in images:
-        # found = False
-        # for keyword in item["keywords"]:
-        #     if query in keyword:
-        #         found = True
-        #         # break # 이미지 하나만 찾고 말거다.
-        # if found:
-        #     image_url = url_for('static', filename=f'img/{item["filename"]}')
-        #     results.append(image_url)
-        
         # pythonic하게 한줄로...
         if any(query in keyword for keyword in item["keywords"]):
             image_url = url_for('static', filename=f'img/{item["filename"]}')
             results.append(image_url)
     
-    # return jsonify({"url": results})  # 순수 BE개발자는 여기까지...
-    return render_template("results.html", query=query, results=results)
+    return render_template("index.html", query=query, results=results)
+
+@app.route('/admin')
+def admin():
+    return render_template("admin.html", images=images)
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files.get('image')
+    keywords = request.form.get('keywords')
+    print(keywords)
+    
+    if file:
+        filename = file.filename
+        filepath = os.path.join('static', 'img', filename)
+        file.save(filepath)
+        images.append({'filename': filename, "keywords": keywords.lower().split(',')})
+    
+    return redirect(url_for('admin'))
+
+@app.route('/update/<filename>', methods=['POST'])
+def update_keywords(filename):
+    new_keywords = request.form.get('keywords')
+    for i in images:
+        if i["filename"] == filename:
+            i["keywords"] = [word.strip() for word in new_keywords.lower().split(',') if len(word.strip())]
+            break
+        
+    return redirect(url_for('admin'))
+
+@app.route('/delete/<filename>')
+def delete_image(filename):
+    print('삭제할파일: ', filename)
+    global images   # 우리의 글로벌 변수를 읽어갈때는 문제가 없음. 단, 수정할꺼면 global로 설정해줘야지만, 해당 변수안의 내용을 수정할 수 있음.
+    
+    # 삭제할거 빼고 나머지를 다시 채움
+    images = [
+        img 
+        for img in images 
+        if img["filename"] != filename
+    ]
+    
+    # 실제로 이미지를 지울거면??
+    filepath = os.path.join('static', 'img', filename)
+    if os.path.exists(filepath):
+        # os.remove(filepath)
+        print(f'{filepath} 지웠다고 치자...')
+    
+    return redirect(url_for('admin'))
 
 if __name__ == "__main__":
     app.run(debug=True)
